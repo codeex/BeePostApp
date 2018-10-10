c72c2ae11ddd8b143144c319b26574083ae76592
webmote <admin@codeex.cn>
2018-10-11 06:12:33 +0800

    超越高山

---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,26 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TabsPageRoutingModule } from './tabs.router.module';
+
+import { TabsPage } from './tabs.page';
+import { MePageModule } from '../me/me.module';
+import { MsgPageModule } from '../msg/msg.module';
+import { HomePageModule } from '../home/home.module';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    TabsPageRoutingModule,
+    HomePageModule,
+    MsgPageModule,
+    MePageModule
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
---
+++ src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+import { HomePage } from '../home/home.page';
+import { MsgPage } from '../msg/msg.page';
+import { MePage } from '../me/me.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirectTo: '/tabs/(home:home)',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        outlet: 'home',
+        component: HomePage
+      },
+      {
+        path: 'msg',
+        outlet: 'msg',
+        component: MsgPage
+      },
+      {
+        path: 'me',
+        outlet: 'me',
+        component: MePage
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/(home:home)',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
