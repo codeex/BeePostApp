20b3118051a7c77a12781d40a0e4891d0c094a2b
webmote <admin@codeex.cn>
2018-10-15 23:25:05 +0800

    增加api访问

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { UserInfoService } from './api/user/user-info.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent implements OnInit  {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private userInfoApi: UserInfoService
   ) {
     this.initializeApp();
   }
@@ -26,6 +28,6 @@ export class AppComponent implements OnInit  {
 
   ngOnInit(): void {
     this.splashScreen.hide();
-    
+    this.userInfoApi.getUserIno('abc');
   }
 }
